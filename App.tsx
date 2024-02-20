11a2e2d8f1b17d6741287d41e04031a9b68b805f
Enzo0708 <enzobiason7@gmail.com>
2024-02-20 14:23:43 -0300

    up

--- App.tsx
+++ App.tsx
@@ -1,24 +1,41 @@
-import {View, Text, Button, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image, Text } from 'react-native';
 
-function App (){
+function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="dark" />
-      <Text>Olá Mundo</Text>
-      <Text>Olá Mundo</Text>
-      <Button title="Clique Aqui" />
-    </View> 
-  )
+      <StatusBar style="auto" />
+        <Image source={require('./assets/Logo.png')} style={styles.logo} />
+        <Text style={styles.text}>Bem vindo(a)!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0000',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    backgroundColor: '#FF0000',
+  },
+
+  whiteBackground: {
+    backgroundColor: '#F6FFFD',
+    width: 360,
+    height: 641,
+    top: 159,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  logo: {
+    width: 200,
+    height: 51,
+    marginTop: 60,
+  },
+  text: {
+    color: '#F6FFFD',
+    fontSize: 20,
+    marginTop: 20,
+  },
 });
 
 export default App;
